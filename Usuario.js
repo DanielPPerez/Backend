1d500388f4c43555e9516bbdb36c926edaf24bbe
DanielPPerez <145521231+DanielPPerez@users.noreply.github.com>
2023-11-15 10:13:07 -0600

    Delete Usuario.js

--- Usuario.js
+++
@@ -1,8 +0,0 @@
-const { default: mongoose } = require("mongoose");
-
-const usuarioSchema = new mongoose.Schema({
-  nombre: String,
-  email: String,
-});
-
-const Usuario = mongoose.model("Usuario", usuarioSchema);
